a240efb2bbbd4f63dd1d9786b8fea946679fbe2a
Dan Pittman <daniel.pittman@intel.com>
2026-02-21 00:16:41 +0000

    functional single 8-d block in asm

--- src/lib.rs
+++ src/lib.rs
@@ -24,27 +24,31 @@ pub fn one_pass_impl<const N: usize>(a: [f32; N], b: [f32; N]) -> f32 {
         mag_b = b[i].mul_add(b[i], mag_b);
         dp = a[i].mul_add(b[i], dp);
     }
-    dp / (mag_a.sqrt() * mag_b.sqrt())
+    dp / (mag_a * mag_b).sqrt()
 }
 
 #[cfg(test)]
 mod test {
-    const ONE_TWO_THREE: [f32; 3] = [1.0, 2.0, 3.0];
+    const ONE_TO_EIGHT: [f32; 8] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+    const RHS: [f32; 8] = [
+        8.938679, 3.701036, 8.291033, 9.767157, 7.0634584, 6.1626635, 3.344915, 4.749503,
+    ];
+
+    const EPSILON: f32 = 0.0009;
+
+    fn is_within_range(a: f32, b: f32) -> bool {
+        (a - b).abs() <= EPSILON
+    }
 
     #[test]
     fn cosine_sim_impls() {
         assert_eq!(
-            super::semantic_impl(ONE_TWO_THREE, ONE_TWO_THREE),
-            super::one_pass_impl(ONE_TWO_THREE, ONE_TWO_THREE)
+            super::semantic_impl(ONE_TO_EIGHT, RHS),
+            super::one_pass_impl(ONE_TO_EIGHT, RHS),
         );
-    }
-
-    #[test]
-    fn temp_dot_prod() {
-        let x: [f32; 8] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
-        assert_eq!(
-            unsafe { super::cosine_sim_asm(x.as_ptr(), x.as_ptr(), 8) },
-            204.0
-        );
+        assert!(is_within_range(
+            super::one_pass_impl(ONE_TO_EIGHT, RHS),
+            unsafe { super::cosine_sim_asm(ONE_TO_EIGHT.as_ptr(), RHS.as_ptr(), 8) },
+        ));
     }
 }
